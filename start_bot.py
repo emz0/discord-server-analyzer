8d9ffab12cc5665befa5da5077cbb4fc43babd7c
emz0 <emz0@github.com>
2018-02-18 01:40:05 +0100

    Add content extractor module to start_bot

--- start_bot.py
+++ start_bot.py
@@ -1,38 +1,12 @@
 import discord
 import asyncio
-import os
-import json
-from db.client import PGClient
+from content_extractor import ContentExtractor
 
 client = discord.Client()
 
-pg_client = PGClient()
-
-
-def parse_custom_emoji(emoji):
-    return emoji[:-1].split(':')[1:]    #return emoji_name, emoji_id
-
-
-async def reactions_to_dict(reactions):
-    reactions_dict = []
-    for r in reactions:
-        r_members = []
-        if r.custom_emoji:
-            emoji = r.emoji.name + '#' + r.emoji.id                
-        else:
-            emoji = r.emoji
-
-        members = await client.get_reaction_users(r, limit=100)
-        members = [m.name + '#' + m.discriminator for m in members]
-
-        reactions_dict.append({'message_id': r.message.id,
-                          'emoji': emoji,
-                          'members': members,
-                         })
-    return reactions_dict
-
 async def save_messages(server_id, channels=[]):
     server = client.get_server(server_id)
+    extractor = ContentExtractor(client)
     all_channels = []
     if channels:
         for c in server.channels:
@@ -40,21 +14,15 @@ async def save_messages(server_id, channels=[]):
                 all_channels.append(c)
     else:
         all_channels = server.channels
-    
+
+    i = 1
     for c in all_channels:
-        async for log in client.logs_from(c, limit=1000000):
-            stringTime = log.timestamp.strftime("%Y-%m-%d %H:%M:%S")
-            author_name = log.author.name
-            author_id = log.author.id
-            discriminator = log.author.discriminator
-            mentions = [m.name+'#'+m.discriminator for m in log.mentions]
-            content = str(log.content)
-            reactions = await reactions_to_dict(log.reactions)
-            pg_client.insert_message(log.id,log.channel.server.id, log.channel.id,
-                                   stringTime, author_id, author_name, content,
-                                   mentions)
-            pg_client.insert_member(author_id, author_name, discriminator)
-            pg_client.insert_reactions(reactions)
+        async for log in client.logs_from(c, limit=1000000000):
+            await extractor.extract_content(log)
+
+            if i % 1000 == 0:
+                print(i)
+            i += 1
 
 @client.event
 async def on_ready():
@@ -62,7 +30,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await save_messages('263420076919750676',['general'])
+    await save_messages('263420076919750676', ['general', 'mapping', 'pokemon', 'esports', 'nsfw'])
     print("DONE!")
     client.logout()
     #get_msgs()
