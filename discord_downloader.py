8dbc1399f660d73509c959b505fe73aac3fd9479
emz0 <emz0@github.com>
2018-02-20 01:17:20 +0100

    Create separate class for message download

---
+++ discord_downloader.py
@@ -0,0 +1,38 @@
+import asyncio
+from content_extractor import ContentExtractor
+
+
+class DiscordDownloader:
+
+    def __init__(self, discord_client, server_id, channels=[]):
+        self.server_id = server_id
+        self.channels = channels
+        self.client = discord_client
+
+    async def save_messages(self):
+        client = self.client
+        channels = self.channels
+
+        server = client.get_server(self.server_id)
+        extractor = ContentExtractor(client)
+        chosen_channels = []
+
+        if self.channels:
+            for c in server.channels:
+                if c.id in channels:
+                    chosen_channels.append(c)
+        else:
+            chosen_channels = server.channels
+
+        i = 1
+        for c in chosen_channels:
+            print('Downloading messages from channel ' + str(c) + '...')
+            c_i = 1
+            async for log in client.logs_from(c, limit=1000000000):
+                await extractor.extract_content(log)
+
+                if i % 1000 == 0:
+                    print('Processed ' + str(i) + ' messages in total')
+                i += 1
+                c_i += 1
+            print('Channel ' + str(c) + ' done. Downloaded ' + str(c_i) + ' messages')
